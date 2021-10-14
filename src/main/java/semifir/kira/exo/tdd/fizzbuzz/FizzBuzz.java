d78d32eddf81753dd8eb2539f19bfc5a8898cd4a
Killian Raoux <killian@semifir.com>
2021-10-14 11:34:11 +0200

    doc(fizzfuzz): Ajout de la documentation pour la fonction FizzBuzz#run

--- src/main/java/semifir/kira/exo/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/semifir/kira/exo/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,30 @@
 package semifir.kira.exo.tdd.fizzbuzz;
 
+import java.util.List;
+
 public class FizzBuzz {
+
+    /**
+     * Cette fonction retourne une liste de String contenant les résultats de la suite fizzbuzz.
+     *
+     * Elle posséde une taille de valeurMaximum.
+     * Chaque String suis la régle suivante:
+     * <ul>
+     *     <li>Si l'index du string est multiple de valeurDeFizz et valeurDeBuzz alors il est égale a FizzBuzz</li>
+     *     <li>Sinon si l'index de string est multiple de valeurDeBuzz alors il est égale a Buzz</li>
+     *     <li>Sinon si l'index de string est multiple de valeurDeFizz alors il est égale a Fizz</li>
+     *     <li>Sinon il est égale a l'index.</li>
+     * </ul>
+     *
+     * @param valeurMaximum valeur jusqu'a la qu'elle aller. Elle représente la taille de la liste de retour.
+     *                      Cette valeur ne peut pas etre négative.
+     * @param valeurDeFizz valeur de fizz.
+     *                     Cette valeur ne peut pas être négative ou nulle.
+     * @param valeurDeBuzz valeur de buzz.
+     *                     Cette valeur ne peut pas être négative ou nulle.
+     * @return la liste des résultats de l'algorithme fizzbuzz.
+     */
+    public List<String> run(int valeurMaximum, int valeurDeFizz, int valeurDeBuzz){
+        return null;
+    }
 }
